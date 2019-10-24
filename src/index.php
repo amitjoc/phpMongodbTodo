10c2ff4abc35d5d4b75d48a05091ff8e6d6e5d08
Amit Joshi <34548066+amitjoc@users.noreply.github.com>
2019-10-25 00:27:43 +0530

    created a project to test new mongodb library

---
+++ src/index.php
@@ -0,0 +1,39 @@
+<?php 
+
+require_once '../vendor/autoload.php';
+
+//making the connection to db
+$mDB = new MongoDB\Client;
+
+
+//listing of db
+$listOFDb = $mDB->listDatabases();
+// var_dump(iterator_to_array($listOFDb));
+
+echo "<code>Database List</code><br />";
+foreach(iterator_to_array($listOFDb,TRUE) as $database){
+    echo $database['name'];
+    echo '<br />';
+}
+echo '<br /><br />';
+echo "<code>Collection List</code><br />";
+//select the db
+$database = $mDB->selectDatabase("test");
+
+$cursor = $database->command(['listCollections'=>1]);
+
+
+ 
+//listing of the collections
+foreach ($cursor as $collection){
+    echo $collection['name'];
+    echo '<br />';
+}
+
+ 
+ 
+ 
+ 
+
+
+?>
